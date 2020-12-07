44331a28aa37a0561b6598d97b30d34106de4c26
javigarcia <javigarciaweb@gmail.com>
2020-12-07 17:16:16 +0100

    Creo la tabla Clientes con su ruta correspondiente

---
+++ database/migrations/2020_12_07_155909_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre', 255);
+            $table->string('apellidos', 255);
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
